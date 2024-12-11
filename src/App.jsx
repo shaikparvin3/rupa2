df00e7fa51e8bde1abd73cbf3d31b8cf39cdc5d5
shaik parvin <parvinshaik9080@gmail.com>
2024-12-11 15:06:47 +0530

    enjoy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 import './App.css'
 
 function App() {
-  const title1=<h3> trending movies</h3>
+  const title1=<h3> Trending movies</h3>
   const title2=<h3> recommended for you</h3>
   const poster1=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQRa8IA_5JL9N2eLjGAXowPcjNXvI1t6YaTw&s" className="movie-poster"/>
   const poster2=<img src="https://stat5.bollywoodhungama.in/wp-content/uploads/2021/12/Pushpa-The-Rise-Part-1-322x423.jpeg"className="movie-poster2"/>
